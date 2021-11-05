760aac872e85bbfd4e462acffe52e65a8e00d68b
zhenkunliu <liufft98@gmail.com>
2021-11-05 15:22:32 +0200

    findOne

--- pages/api/videos/[id].js
+++ pages/api/videos/[id].js
@@ -12,38 +12,11 @@ export default async function handler(req, res) {
     switch (method) {
         case 'GET' /* Get a model by its ID */:
             try {
-                const video = await Video.findById(id)
+                const video = await Video.findOne({"id":id})
                 if (!video) {
                     return res.status(400).json({ success: false })
                 }
                 res.status(200).json({ success: true, data: video })
-            } catch (error) {
-                res.status(400).json({ success: false })
-            }
-            break
-
-        case 'PUT' /* Edit a model by its ID */:
-            try {
-                const video = await Video.findByIdAndUpdate(id, req.body, {
-                    new: true,
-                    runValidators: true,
-                })
-                if (!video) {
-                    return res.status(400).json({ success: false })
-                }
-                res.status(200).json({ success: true, data: video })
-            } catch (error) {
-                res.status(400).json({ success: false })
-            }
-            break
-
-        case 'DELETE' /* Delete a model by its ID */:
-            try {
-                const deletedVideo = await Video.deleteOne({ id: id })
-                if (!deletedVideo) {
-                    return res.status(400).json({ success: false })
-                }
-                res.status(200).json({ success: true, data: {} })
             } catch (error) {
                 res.status(400).json({ success: false })
             }
