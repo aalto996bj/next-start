e0347b86989021312f2d1789167c013d9ca88c53
xzifan <xiao@zifan.dev>
2021-11-14 14:48:54 +0800

    ssl

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+var https = require('https');
+var fs = require('fs');
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({ dev, dir: __dirname })
 const handle = app.getRequestHandler()
 
 var port = 3000
@@ -11,8 +13,13 @@ if (!dev) {
   port = 80
 }
 
+var options = {
+  key: fs.readFileSync('publick.key.pem'),
+  cert: fs.readFileSync('domain.cert.pem'),
+};
+
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(options, (req, res) => {
     // Be sure to pass `true` as the second argument to `url.parse`.
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
